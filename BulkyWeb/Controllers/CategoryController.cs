f66a5668a10f869217635c97223c2eb18e844540
iamakhildabral <akhildabral02@gmail.com>
2023-08-15 10:40:25 +0530

    returing direct htlm content in IActionResult

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {   
+            var categoryList = _db.Categories.ToList();
+            Category category2 = null;
+
+            //return View(categoryList);
+ 
+            return Content("No Element Found");
+            
+        }
+    }
+}

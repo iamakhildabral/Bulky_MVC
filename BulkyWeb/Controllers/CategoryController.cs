92b0486c69945feaf96a590f7fdf5e8b2a70d5f5
iamakhildabral <akhildabral02@gmail.com>
2023-08-17 13:18:16 +0530

    added model only verification of the inp aling with Adding ModelValue

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -11,6 +11,9 @@ namespace BulkyWeb.Controllers
         {
             _db = db;
         }
+        /*
+         * This Method is for displaing all the Cat List on the Frontend to users
+         */
         public IActionResult Index()
         {   
             var categoryList = _db.Categories.ToList();
@@ -31,6 +34,40 @@ namespace BulkyWeb.Controllers
            
                 return View();
 
+        }
+
+        /*
+         * This is for the POST request made by the previous method
+         */
+
+        [HttpPost]
+        public IActionResult Create(Category categoryObj)
+        {
+            /*
+             * For checking whether user has added the same parameters
+             * for both name and category.
+             */
+            if(categoryObj.Name == categoryObj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Both DisplayOrder and Name cannot have same values:");
+            }
+
+            if (categoryObj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("",$"The Name cannot be :\"{categoryObj.Name}\"");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(categoryObj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                return View();
+            }
         }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace BulkyWeb.Models
 {
@@ -7,10 +8,15 @@ namespace BulkyWeb.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
+
         public required string Name { get; set; }
+
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Order must in the range of 1 to 100")]
         public int DisplayOrder { get; set; }
     }
 }
